5df48c528cfe6c76abd6953d38b2b3fad8757a05
Thomas Meerpohl <thomas.meerpohl@gmail.com>
2024-04-16 14:12:18 +0200

    Removed delay from keyboard plugin. Added small fixes to build project

--- keyboard/rollup.config.js
+++ keyboard/rollup.config.js
@@ -4,7 +4,7 @@ export default {
     {
       file: 'dist/plugin.js',
       format: 'iife',
-      name: 'capacitorKeyboard',
+      name: 'meerpohlKeyboard',
       globals: {
         '@capacitor/core': 'capacitorExports',
       },
